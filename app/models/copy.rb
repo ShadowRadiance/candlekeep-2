63366e30338ab0151d34e2e3e0f779d849235eaf
Megan McVey <shadowradiance@gmail.com>
2018-04-14 05:15:27 -0400

    Placeholder for "Available" Copies
    Added in a placeholder to show number available vs max number of copies.

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,3 +1,8 @@
 class Copy < ApplicationRecord
   belongs_to :book
+
+  # faking the available scope for now since we don't have checkouts yet
+  # but it should be something like this
+  # scope :available, -> { includes(:checkouts).where(checkouts: {id: nil}) }
+  scope :available, -> { limit(5) }
 end
