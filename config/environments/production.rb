6f74a27cd51d16d063c3ecda7be116e408b4f01f
Megan McVey <shadowradiance@gmail.com>
2018-04-16 19:15:20 -0400

    Whoops

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'stark-chamber-52048.heroku.com' }
+  config.action_mailer.default_url_options = { host: 'stark-chamber-52048.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 25,
