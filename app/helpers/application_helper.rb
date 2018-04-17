772c770d5523eec284fabe1d0ac3a1438d90a6d5
Megan McVey <shadowradiance@gmail.com>
2018-04-17 02:03:06 -0400

    Display the user-local time in the header
    As reminder to check times/zones while handling checkouts/checkins/due dates
    
    Also pretty up the header user nav a little

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,44 @@ module ApplicationHelper
     end
   end
 
+  # I'm not 100% sure I want these in the Helper or the Controller
+  # I think it's a helper, because only the Views care about the UserNavigation
+
+  def generate_user_nav
+    if user_signed_in?
+      member_nav(current_user)
+    else
+      guest_nav
+    end
+  end
+
+  private
+
+  def member_nav(user)
+    {
+      time: "Local: #{Time.current.in_time_zone(user.time_zone).to_s(:long)}",
+      dropdown: {
+        type: 'Member',
+        name: current_user.email,
+        links: [
+          link_to('Edit profile', edit_user_registration_path, class: 'dropdown-item'),
+          link_to('Logout', destroy_user_session_path, method: :delete, class: 'dropdown-item')
+        ]
+      }
+    }
+  end
+
+  def guest_nav
+    {
+      time: "Server: #{Time.current.to_s(:long)}",
+      dropdown: {
+        type: 'Guest',
+        name: 'Guest',
+        links: [
+          link_to('Sign up', new_user_registration_path, class: 'dropdown-item'),
+          link_to('Login', new_user_session_path, class: 'dropdown-item')
+        ]
+      }
+    }
+  end
 end
