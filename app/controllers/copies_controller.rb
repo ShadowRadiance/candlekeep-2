6c4d2a8f5f45da44216e6705553b1bf7f705822d
Megan McVey <shadowradiance@gmail.com>
2018-04-19 09:13:08 -0400

    Branch-relative Copies on Book Detail
    Display branches on book detail
    Allow admin to create new copies at branch

--- app/controllers/copies_controller.rb
+++ app/controllers/copies_controller.rb
@@ -3,27 +3,26 @@ class CopiesController < ApplicationController
   before_action :require_admin!
 
   def create
-    @book = Book.find(params[:book_id])
-    @copy = @book.copies.create
-    if @copy.valid?
-      redirect_to @book, notice: 'Added a new copy'
+    book = Book.find(params[:book_id])
+    branch = Branch.find(params[:branch_id])
+    copy = book.copies.create(branch: branch)
+    if copy.valid?
+      redirect_to book, notice: 'Added a new copy'
     else
-      redirect_to @book, alert: 'Error adding copy: ' + @copy.errors.full_messages.to_sentence
+      redirect_to book, alert: 'Error adding copy: ' + copy.errors.full_messages.to_sentence
     end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @copy = @book.copies.find(params[:id])
+    @copy = Copy.find(params[:id])
     @copy.update(destroyed_at: Time.current)
-    redirect_to @book, notice: 'Copy deleted'
+    redirect_to @copy.book, notice: 'Copy deleted'
   end
 
   def restore
-    @book = Book.find(params[:book_id])
-    @copy = @book.destroyed_copies.find(params[:id])
+    @copy = Copy.find(params[:id])
 
     @copy.update(destroyed_at: nil)
-    redirect_to @book, notice: 'Copy restored!'
+    redirect_to @copy.book, notice: 'Copy restored!'
   end
 end
--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,3 +1,23 @@
 class Branch < ApplicationRecord
-  
+
+  has_many :copies
+  has_many :books, through: :copies
+
+
+  def carries_copy_of?(book)
+    copies_of(book).count.positive?
+  end
+
+  def carries_available_copy_of?(book)
+    available_copies_of(book).count.positive?
+  end
+
+  def copies_of(book)
+    book.copies_at(self)
+  end
+
+  def available_copies_of(book)
+    copies_of(book).available
+  end
+
 end
