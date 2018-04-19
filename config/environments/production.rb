e681efdd81e8cf78e4f3c2aa3626a5b61080111b
Megan McVey <shadowradiance@gmail.com>
2018-04-19 11:45:33 -0400

    Update Heroku Sendgrid Mailer Info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { 
-    host: 'stark-chamber-52048.herokuapp.com',
+    host: 'secret-tundra-89841.herokuapp.com',
     protocol: 'https'
   }
   ActionMailer::Base.smtp_settings = {
