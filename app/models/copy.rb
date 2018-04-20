5bff278300fbd8f016d1f5249f7bbfd909a92a0a
Megan McVey <shadowradiance@gmail.com>
2018-04-20 00:27:37 -0400

    Update copy when updating checkout

--- app/models/copy.rb
+++ app/models/copy.rb
@@ -4,9 +4,9 @@ class Copy < ApplicationRecord
   belongs_to :branch
   belongs_to :checked_out_by, class_name: 'User', optional: true
 
-  has_many :checkouts, -> { where(checked_in_at: nil) }
 
   scope :available, -> { includes(:checkouts).where(checkouts: {id: nil}) }
+  before_validation :set_due_date
 
   def available?
     checkouts.count.zero?
@@ -22,5 +22,17 @@ class Copy < ApplicationRecord
 
   def current_borrower
     checkouts.first&.user
+  end
+
+  private
+
+  def set_due_date
+    if checked_out_at.nil?
+      self.due_at = nil
+    else
+      # book must be returned one week after checkout date
+      local_checkout = checked_out_at.in_time_zone(checked_out_by.time_zone)
+      self.due_at = local_checkout.end_of_day + 1.week
+    end
   end
 end
